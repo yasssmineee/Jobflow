ee5c186e4dd0e5fb0b900ae88349c6294c983edf
yasssmineee <yasmine.dridi@esprit.tn>
2024-03-03 23:45:01 +0100

    rourou mawjoud

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,14 +5,35 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-
+use Symfony\Component\Security\Core\Security;
+use App\Entity\Societe;
 class HomeController extends AbstractController
 {
     #[Route('/home', name: 'app_home')]
-    public function index(): Response
+    public function index(Security $security): Response
     {
-        return $this->render('home/sanslogin.html.twig', [
-            'controller_name' => 'HomeController',
+        $user = $security->getUser();
+        $username = $user ? $user->getUserIdentifier() : null;
+
+        return $this->render('home/societelogin.html.twig', [
+            'username' => $username,
+        ]);
+    }
+    
+    #[Route('/profile', name: 'profile')]
+    public function profile(): Response
+    {
+         /** @var UserInterface|null $user */
+        $user = $this->getUser();
+
+        // Check if the user is logged in and has a Societe associated with it
+        if (!$user || !$user->getSociete()) {
+            throw $this->createNotFoundException('User not found or no Societe associated with the user.');
+        }
+
+        // Render the profile template with the associated Societe information
+        return $this->render('profile/index.html.twig', [
+            'user' => $user,
         ]);
     }
 }
